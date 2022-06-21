9598252b302e5d7c5a44d0aa4932b7dec48dfc91
Anderson Aquino <andersonaquino93@gmail.com>
2022-06-20 23:42:18 -0300

    chore: add global styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import { ThemeProvider } from 'styled-components';
+
+import GlobalStyles from './assets/styles/global';
+import defaultTheme from './assets/styles/themes/default';
+
 function App() {
   return (
-    <div className="App">
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles />
       <h1>My Contacts</h1>
-    </div>
+    </ThemeProvider>
   );
 }
 
