18169bc1a44ae8c1627ea03cf7d934aebc479b11
Anderson Aquino <andersonaquino93@gmail.com>
2022-06-21 00:38:55 -0300

    feat: add ContactList component header (WIP)

--- src/assets/styles/themes/default.js
+++ src/assets/styles/themes/default.js
@@ -1,3 +1,11 @@
 export default {
-  backgroundColor: '#F6F5FC',
+  colors: {
+    background: '#F6F5FC',
+    primary: {
+      dark: '#3346f0',
+      light: '#6674f4',
+      lighter: '#e0e3ff',
+      main: '#5061fc',
+    },
+  },
 };
