37ba2fa86c34f1372458ece524f718c847b0721a
Anderson Aquino <andersonaquino93@gmail.com>
2022-06-21 10:02:55 -0300

    chore: add common components Input and Select

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,7 +1,19 @@
 import PageHeader from '../../components/PageHeader';
+import { Input, Select } from '../../components/common';
 
 export default function NewContact() {
   return (
-    <PageHeader title="Novo Contato" />
+    <>
+      <PageHeader title="Novo Contato" />
+
+      <Input type="text" placeholder="Nome" />
+
+      <Select>
+        <option value={1}>1</option>
+        <option value={1}>1</option>
+        <option value={1}>1</option>
+        <option value={1}>1</option>
+      </Select>
+    </>
   );
 }
