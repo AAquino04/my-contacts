716af661c9ffd3cfa806b5dc4445b31f94654c4e
Anderson Aquino <andersonaquino93@gmail.com>
2022-07-01 09:00:04 -0300

    refactor: add Input components error status and convert Contact form to controlled component

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import FormGroup from '../FormGroup';
@@ -7,18 +7,41 @@ import { Button, Input, Select } from '../common';
 import { Form, ButtonContainer } from './styles';
 
 export default function ContactForm({ buttonLabel }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  // const [contactOrigin, setContactOrigin] = useState('');
+
   return (
     <Form>
       <FormGroup>
-        <Input type="text" placeholder="Nome" />
+        <Input
+          type="text"
+          placeholder="Nome"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+      </FormGroup>
+
+      <FormGroup
+        error="O formato do e-mail é inválido."
+      >
+        <Input
+          error
+          type="email"
+          placeholder="E-mail"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
       </FormGroup>
 
       <FormGroup>
-        <Input type="email" placeholder="E-mail" />
-      </FormGroup>
-
-      <FormGroup>
-        <Input type="text" placeholder="Telefone" />
+        <Input
+          type="text"
+          placeholder="Telefone"
+          value={phone}
+          onChange={(event) => setPhone(event.target.value)}
+        />
       </FormGroup>
 
       <FormGroup>
