496816b6ba436a3f0286d6d6e4976946491866cf
Anderson Aquino <andersonaquino93@gmail.com>
2022-07-30 01:03:13 -0300

    refactor: convert get all contacts to async/await

--- ui/src/pages/Home/index.js
+++ ui/src/pages/Home/index.js
@@ -24,21 +24,23 @@ export default function Home() {
   )), [contacts, searchTerm]);
 
   useEffect(() => {
-    setIsLoading(true);
+    async function loadContacts() {
+      try {
+        setIsLoading(true);
 
-    fetch(`http://localhost:3001/contacts?orderBy=${orderBy}`)
-      .then(async (response) => {
+        const response = await fetch(`http://localhost:3001/contacts?orderBy=${orderBy}`);
         await delay(2000);
 
         const contactsList = await response.json();
         setContacts(contactsList);
-      })
-      .catch((error) => {
-        console.log('error', error);
-      })
-      .finally(() => {
+      } catch (error) {
+        console.log(error);
+      } finally {
         setIsLoading(false);
-      });
+      }
+    }
+
+    loadContacts();
   }, [orderBy]);
 
   function handleChangeSearchTerm(event) {
