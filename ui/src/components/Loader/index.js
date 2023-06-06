808bd9edba53d5c89e5ec512f167060c5fc8765a
Anderson Aquino <andersonaquino93@gmail.com>
2023-06-06 00:26:03 -0300

    style: add animation to loader

--- ui/src/components/Loader/index.js
+++ ui/src/components/Loader/index.js
@@ -4,16 +4,18 @@ import PropTypes from 'prop-types';
 import ReactPortal from '../ReactPortal';
 import Spinner from '../Spinner';
 
+import useAnimatedUnmount from '../../hooks/useAnimatedUnmount';
+
 import { Overlay } from './styles';
 
 export default function Loader({ isLoading }) {
-  if (!isLoading) {
-    return null;
-  }
+  const { shouldRender, animatedElementRef } = useAnimatedUnmount(isLoading);
+
+  if (!shouldRender) return null;
 
   return (
     <ReactPortal containerId="loader-root">
-      <Overlay>
+      <Overlay isLeaving={!isLoading} ref={animatedElementRef}>
         <Spinner size={90} />
       </Overlay>
     </ReactPortal>
